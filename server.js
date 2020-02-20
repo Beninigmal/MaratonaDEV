1118332e428e9629e33f8e248f69c32a216e17f0
Beni <beninigmal@gmail.com>
2020-02-20 17:45:41 -0300

    adicionado o bando de dados no git

--- server.js
+++ server.js
@@ -9,6 +9,17 @@ server.use(express.static('public'))
 //habilitar body do formulário
 server.use(express.urlencoded({ extended: true }))
 
+//Configurar a conexão com o banco de dados
+const Pool = require('pg').Pool
+const db = new Pool({
+    user: 'postgres',
+    password: '18102010',
+    host: 'localhost',
+    port: 5432,
+    database: 'doe'
+})
+
+
 //configurando a template engine
 
 const nunjunks = require("nunjucks")
@@ -19,24 +30,7 @@ nunjunks.configure("./", {
 
 
 //Lista de doadores: vetor ou array
-const donors = [
-    {
-        name: "Diego Fernandes",
-        blood: "AB+"
-    },
-    {
-        name: "Lucas Araújo",
-        blood: "O+"
-    },
-    {
-        name: "Aiz Lima",
-        blood: "A+"
-    },
-    {
-        name: "Ully Lima",
-        blood: "A-"
-    }
-]
+
 
 
 
@@ -44,29 +38,53 @@ const donors = [
 
 //configurar a apresentação da página
 server.get("/", function(req, res) {
-    return res.render("index.html", {donors})
+
+
+    db.query("SELECT * FROM donors", function(err, result) {
+        if (err) return res.send("Erro de banco de dados!")
+
+        const donors = result.rows
+        return res.render("index.html", { donors })
+    })
+
+
 })
 
-server.post('/', function(req, res){
+server.post('/', function(req, res) {
     //pegar dados do formulário
     const name = req.body.name
     const email = req.body.email
     const blood = req.body.blood
 
-    //colcar valores dentro do array
-    donors.push({
-        name: name,
-        blood: blood,
-    })
+    if (name == "" || email == "" || blood == "") {
+        return res.send("Todos os campos são obrigatórios!")
+    }
 
-    return res.redirect("/")
+    //colocar valores dentro do banco de dados.
+    const query = `
+        INSERT INTO donors ("name", "email", "blood") 
+        VALUES ($1, $2, $3)`
+
+    const values = [name, email, blood]
+
+    db.query(query, values, function(err) {
+            //fluxo de erro
+            if (err) return res.send('Erro no banco de dados.')
+
+            //fluxo ideal
+            return res.redirect("/")
+
+        })
+        // donors.push({
+        //     name: name,
+        //     blood: blood,
+        // })
+
+
 })
 
 
 // ligar o servidor e permitir o acesso na porte 3000
-server.listen(3000, function(){
+server.listen(3000, function() {
     console.log('Iniciado o servidor...')
 })
-
-//iniciando banco de dados
-// tempo parado de aula 1:09
